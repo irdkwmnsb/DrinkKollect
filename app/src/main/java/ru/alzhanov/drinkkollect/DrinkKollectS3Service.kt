a708d33d0aa355da8b318ef705c671c265c0893d
greenIrina <irinat1230@gmail.com>
2023-05-12 17:03:42 +0300

    merged and cried

--- app/src/main/java/ru/alzhanov/drinkkollect/DrinkKollectS3Service.kt
+++ app/src/main/java/ru/alzhanov/drinkkollect/DrinkKollectS3Service.kt
@@ -1,10 +1,16 @@
 package ru.alzhanov.drinkkollect
 
+import android.net.Uri
 import com.amazonaws.ClientConfiguration
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.regions.Region
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.S3ClientOptions
+import io.grpc.CallOptions
+import io.grpc.ClientCall
+import io.grpc.Metadata
+import io.grpc.MethodDescriptor
+import io.grpc.netty.NettyChannelBuilder
 import java.util.Date
 
 class DrinkKollectS3Service(host: String, accessKey: String, secretKey: String) {
@@ -19,5 +25,21 @@ class DrinkKollectS3Service(host: String, accessKey: String, secretKey: String) 
 
     fun getUrl(bucket: String, key: String): String {
         return s3?.generatePresignedUrl(bucket, key, Date(Date().time + 60000))?.toString() ?: ""
+    }
+    fun setUrl(image: Uri?) {
+        val metadata = Metadata()
+        metadata.put(
+            Metadata.Key.of(
+                "content-type",
+                Metadata.ASCII_STRING_MARSHALLER)
+            , "application/octet-stream"
+        )
+        val newBuilder = MethodDescriptor.newBuilder<ByteArray, String>().build()
+        NettyChannelBuilder.forTarget("WHAT DO I PUT HERE").build()
+            .newCall(newBuilder, CallOptions.DEFAULT)
+            .start(PseudoListener(), metadata)
+    }
+    private class PseudoListener : ClientCall.Listener<String>() {
+
     }
 }
